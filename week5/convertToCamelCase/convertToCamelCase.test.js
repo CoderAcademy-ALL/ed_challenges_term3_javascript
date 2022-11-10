0174cbacdb87d1703044b133c6f8fba31b6b97d8
Jordy Williams <jordy_williams@hotmail.co.uk>
2022-11-11 10:47:49 +1100

    test: updates tests to test `toCamelCaseArrayBased`

--- week5/convertToCamelCase/convertToCamelCase.test.js
+++ week5/convertToCamelCase/convertToCamelCase.test.js
@@ -1,23 +1,33 @@
-const { toCamelCase } = require("./convertToCamelCase.js");
+const {
+  toCamelCase,
+  toCamelCaseArrayBased,
+} = require("./convertToCamelCase.js");
 
 describe("toCamelCase", () => {
   it("returns an empty string when passed an empty string", () => {
     expect(toCamelCase("")).toBe("");
+    expect(toCamelCaseArrayBased("")).toBe("");
   });
 
   it("converts kebab case", () => {
     expect(toCamelCase("foo-bar")).toBe("fooBar");
+    expect(toCamelCaseArrayBased("foo-bar")).toBe("fooBar");
   });
 
   it("converts snake case", () => {
     expect(toCamelCase("hello_there")).toBe("helloThere");
+    expect(toCamelCaseArrayBased("hello_there")).toBe("helloThere");
   });
 
   it("converts multiple words in snake case", () => {
     expect(toCamelCase("this_is_quite_long")).toBe("thisIsQuiteLong");
+    expect(toCamelCaseArrayBased("this_is_quite_long")).toBe("thisIsQuiteLong");
   });
 
   it("converts upcased first word", () => {
     expect(toCamelCase("First_Word_capital")).toBe("FirstWordCapital");
+    expect(toCamelCaseArrayBased("First_Word_capital")).toBe(
+      "FirstWordCapital"
+    );
   });
 });
