05077cc260233fe80dfdcd371accdb7d2d4f6d32
Jordy Williams <jordy_williams@hotmail.co.uk>
2022-11-18 10:46:21 +1100

    feat: less verbose variables

--- week6/atomicBlonde/atomicBlonde.js
+++ week6/atomicBlonde/atomicBlonde.js
@@ -1,3 +1,4 @@
+// Instead of this approach one could also iterate over a numeric string thus only needing one loop.
 // mathOperation to be a function returning one number with two input numbers
 const mathOperationOnDigits = (n, mathOperation) =>
   Array.from(String(n), Number).reduce((prev, current) =>
@@ -5,9 +6,9 @@ const mathOperationOnDigits = (n, mathOperation) =>
   );
 
 const atomicBlonde = (num) => {
-  const sumDigits = mathOperationOnDigits(num, (val1, val2) => val1 + val2);
-  const productDigits = mathOperationOnDigits(num, (val1, val2) => val1 * val2);
-  return sumDigits == productDigits;
+  return (
+    mathOperationOnDigits(num, (val1, val2) => val1 + val2) ==
+    mathOperationOnDigits(num, (val1, val2) => val1 * val2)
+  );
 };
-
 module.exports = { atomicBlonde };
