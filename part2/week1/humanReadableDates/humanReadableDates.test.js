3e519b4be077189bfe1f9cc56a0609b929d6de59
Jordy Williams <jordy_williams@hotmail.co.uk>
2023-01-13 09:31:55 +1100

    test: adds tests for `humanReadableMath` method

--- part2/week1/humanReadableDates/humanReadableDates.test.js
+++ part2/week1/humanReadableDates/humanReadableDates.test.js
@@ -1,4 +1,4 @@
-const { humanReadable } = require("./humanReadableDates.js");
+const { humanReadable, humanReadableMath } = require("./humanReadableDates.js");
 
 describe("tests", function () {
   it("should format correctly", function () {
@@ -12,6 +12,16 @@ describe("tests", function () {
     expect(humanReadable(86399)).toStrictEqual("23:59:59");
     expect(humanReadable(86400)).toStrictEqual("24:00:00");
     expect(humanReadable(359999)).toStrictEqual("99:59:59");
+    expect(humanReadableMath(0)).toStrictEqual("00:00:00");
+    expect(humanReadableMath(59)).toStrictEqual("00:00:59");
+    expect(humanReadableMath(60)).toStrictEqual("00:01:00");
+    expect(humanReadableMath(90)).toStrictEqual("00:01:30");
+    expect(humanReadableMath(3599)).toStrictEqual("00:59:59");
+    expect(humanReadableMath(3600)).toStrictEqual("01:00:00");
+    expect(humanReadableMath(45296)).toStrictEqual("12:34:56");
+    expect(humanReadableMath(86399)).toStrictEqual("23:59:59");
+    expect(humanReadableMath(86400)).toStrictEqual("24:00:00");
+    expect(humanReadableMath(359999)).toStrictEqual("99:59:59");
   });
 
   function solution(seconds) {
@@ -36,7 +46,9 @@ describe("tests", function () {
       const seconds = getRandomIntInclusive(0, 359999);
       const expected = solution(seconds);
       const actual = humanReadable(seconds);
+      const actualMath = humanReadableMath(seconds);
       expect(actual).toStrictEqual(expected);
+      expect(actualMath).toStrictEqual(expected);
     }
   });
 });
